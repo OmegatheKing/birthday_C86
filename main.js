09612c21bdc85fc2ae909345ff195ad7e04222d3
OmegatheKing <110073349+OmegatheKing@users.noreply.github.com>
2022-11-26 19:27:44 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+var canvas = new fabric.Canvas('myCanvas');
+ var x= document.getElementById("myAudio");
+
+function new_image()
+{
+fabric.Image.fromURL('BirthdayImage.jpg', function(Img){
+blockimgobject=Img;
+blockimgobject.scaleToWidth(700);
+blockimgobject.scaleToHeight(510);
+blockimgobject.set({
+top:0,
+left:0
+});
+canvas.add(blockimgobject);
+});
+}
+function playSound(){
+	x.play();
+}
